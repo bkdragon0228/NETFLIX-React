6225a65263af6aa82f698f21e2e8f746423bc6fc
bkdragon0228 <rlaqjarb2067@naver.com>
2022-10-19 14:43:06 +0900

    feat : 상세페이지
    상세페이지 제작

--- netflix/src/Pages/DetailPage/index.js
+++ netflix/src/Pages/DetailPage/index.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import axios from '../../api/axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const DetailPage = () => {
-    return <div></div>;
+    let { movieId } = useParams();
+    const [movie, setMovie] = useState({});
+
+    useEffect(() => {
+        fetchData();
+    }, [movieId]);
+
+    const fetchData = async () => {
+        const request = await axios.get(`/movie/${movieId}`);
+        setMovie(request.data);
+    };
+
+    if (!movie) return <div>...loading</div>;
+
+    return (
+        <section>
+            <img
+                className="modal__poster-img"
+                alt="image"
+                src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+            ></img>
+        </section>
+    );
 };
 
 export default DetailPage;
--- netflix/src/Pages/SearchPage/index.js
+++ netflix/src/Pages/SearchPage/index.js
@@ -1,10 +1,12 @@
 import axios from '../../api/axios';
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import './SearchPage.css';
+import { useDebounce } from '../../hooks/useDebounce';
 
 const SearchPage = () => {
     const [searchResults, setSearchResults] = useState([]);
+    const navigate = useNavigate();
 
     const useQuery = () => {
         return new URLSearchParams(useLocation().search); //현재 url 정보 가져오기
@@ -13,15 +15,16 @@ const SearchPage = () => {
     let query = useQuery();
 
     const searchTerm = query.get('q'); // useLocation에서 원하는 부분만 가져오기
-    console.log('searchTerm', searchTerm);
+    const deboucedSearchTerm = useDebounce(searchTerm, 800);
 
     useEffect(() => {
-        if (searchTerm) {
-            fetchSearchMovie(searchTerm);
+        if (deboucedSearchTerm) {
+            fetchSearchMovie(deboucedSearchTerm);
         }
-    }, [searchTerm]);
+    }, [deboucedSearchTerm]);
 
     const fetchSearchMovie = async (searchTerm) => {
+        console.log('deboucedSearchTerm', deboucedSearchTerm);
         try {
             const req = await axios.get(
                 `/search/multi?include_adult=false&query=${searchTerm}`
@@ -46,8 +49,11 @@ const SearchPage = () => {
                             movie.backdrop_path;
 
                         return (
-                            <div className="movie">
-                                <div className="movie__column-poster">
+                            <div className="movie" key={movie.id}>
+                                <div
+                                    className="movie__column-poster"
+                                    onClick={() => navigate(`/${movie.id}`)}
+                                >
                                     <img
                                         src={movieImageURL}
                                         alt="movie"
@@ -63,14 +69,15 @@ const SearchPage = () => {
             <section className="no-results">
                 <div className="no-result__text">
                     <p>
-                        찾고자하는 검색어"{searchTerm}"에 맞는 영화가 없습니다."
+                        찾고자하는 검색어"{deboucedSearchTerm}"에 맞는 영화가
+                        없습니다."
                     </p>
                 </div>
             </section>
         );
     };
 
-    return renderSearchResults();
+    return searchTerm ? renderSearchResults() : <div></div>;
 };
 
 export default SearchPage;
